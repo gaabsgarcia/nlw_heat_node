3f269940d17207fac79d33cd01f7f6a1bc60cae7
gaabsgarcia <gabriella.garciass16@gmail.com>
2021-10-20 15:02:59 -0300

    'feat: stage 1 completed'

--- src/services/ProfileUserService.ts
+++ src/services/ProfileUserService.ts
@@ -1,7 +1,7 @@
 import prismaClient from "../prisma";
 
 class ProfileUserService {
-  async execute() {
+  async execute(user_id: string) {
     const user = await prismaClient.user.findFirst({
       where: {
         id: user_id,
